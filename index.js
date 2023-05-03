dd98059ee0d9bef6e22d6b00fdaff55e9c61e4f9
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-05-02 22:29:37 -0500

    able to save but not pull

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const dataD = require("./db/db.json")
+const { writeFile } = require("fs")
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,42 +15,61 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-// GET request for reviews
-app.get('/api/reviews', (req, res) => {
-  res.status(200).json(reviews);
+// GET request for notes
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(dataD);
 });
 
-// POST request to add a review
-// NOTE: Data persistence isn't set up yet, so this will only exist in memory until we implement it
-app.post('/api/reviews', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
-
-  // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
-
-  // If all the required properties are present
-  if (product && review && username) {
-    // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      review_id: uuid(),
-    };
-
-    const response = {
-      status: 'success',
-      body: newReview,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
+// GET request for a single note
+app.get('/api/notes/:note_id', (req, res) => {
+  if (req.params.note_id) {
+    console.info(`${req.method} request received to get a single a note`);
+    const noteId = req.params.note_id;
+    for (let i = 0; i < notes.length; i++) {
+      const currentNotes = notes[i];
+      if (currentNotes.note_id === noteId) {
+        res.json(currentNotes);
+        return;
+      }
+    }
+    res.status(404).send('Note not found');
   } else {
-    res.status(500).json('Error in posting review');
+    res.status(400).send('Note ID not provided');
   }
 });
+
+// POST request for notes
+app.post('/api/notes', (req, res) => {
+  
+const newNote = req.body
+
+  dataD.push(newNote);
+const noteString = JSON.stringify(dataD)
+    // Write the string to a file
+    writeFile(path.join(__dirname, `./db/db.json`), noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Note for ${newNote.product} has been written to JSON file`
+          )
+    );
+
+  res.status(200).json(dataD);
+});
+
+// DELETE request for notes
+app.delete('/api/notes', (req, res) => {
+  
+  
+
+  
+  res.status(200).json(dataD);
+});
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
